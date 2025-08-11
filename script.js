c4e125d098f5cefecc4c0a238c820e62a5d64257
Chance C. <chancec0373@bxcsm.org>
2025-08-11 10:26:05 +0000

    Refactor HTML structure and add testimonials section; update JavaScript for dynamic reviews and enhance CSS styles.

--- script.js
+++ script.js
@@ -1,36 +1,139 @@
-console.log("Hello, World!");
+const reviews = [
+    {
+        id: 1,
+        name: 'Susan Smith',
+        job: 'Web Designer',
+        img: 'images/susan.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    },
+    {
+        id: 2,
+        name: 'Anna Johnson',
+        job: 'Web Developer',
+        img: 'images/anna.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    },
+    {
+        id: 3,
+        name: 'Peter Jones',
+        job: 'Teacher',
+        img: 'images/peter.jpg',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    }
+];
 
-//Strings have quotes & numbers don't//
-let myTest="123";
-console.log(typeof myTest);
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
-console.log(10 + " eggs");
-console.log(10 + 8 + " eggs");
-console.log("eggs" + 10 + 8);
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-//Both conditions need to be true//
-console.log(7>3 && 5>2);
+let currentItem = 0;
 
-//Only one condition must be true//
-console.log(10>3||(10>12))
+// Load initial item
+window.addEventListener('DOMContentLoaded', function () {
+    showPerson(currentItem);
+});
 
-//Logical NOT//
-console.log(!5>3)
-
-function introduction(name,age){
-    console.log("Hello, my name is " + name + " & I'm " + age + " years old.")
+function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
 }
 
-introduction("Chance" , 14)
+nextBtn.addEventListener('click', function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
 
-const hour = new Date().getHours();
-let greeting = "";
+prevBtn.addEventListener('click', function () {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
 
-if (hour<12){
-    greeting = "Good Morning🌅";
-}else if (hour<18){
-    greeting = "Good Afternoon⛅"
-}else{
-    greeting = "Good Evening🌙"
+function flashButton(button) {
+  button.classList.add('clicked');
+  setTimeout(() => {
+    button.classList.remove('clicked');
+  }, 200); // 200ms flash
 }
-document.getElementById("greeting").innerText = greeting
+
+nextBtn.addEventListener('click', function () {
+  flashButton(nextBtn);
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function () {
+  flashButton(prevBtn);
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+// Highlight active navbar link
+document.addEventListener('DOMContentLoaded', () => {
+  const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+  
+  document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    const linkPage = link.getAttribute('href').split('/').pop();
+    if (linkPage === currentPage) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+});
+
+
+// console.log("Hello, World!");
+
+// //Strings have quotes & numbers don't//
+// let myTest="123";
+// console.log(typeof myTest);
+
+// console.log(10 + " eggs");
+// console.log(10 + 8 + " eggs");
+// console.log("eggs" + 10 + 8);
+
+// //Both conditions need to be true//
+// console.log(7>3 && 5>2);
+
+// //Only one condition must be true//
+// console.log(10>3||(10>12))
+
+// //Logical NOT//
+// console.log(!5>3)
+
+// function introduction(name,age){
+//     console.log("Hello, my name is " + name + " & I'm " + age + " years old.")
+// }
+
+// introduction("Chance" , 14)
+
+// const hour = new Date().getHours();
+// let greeting = "";
+
+// if (hour<12){
+//     greeting = "Good Morning🌅";
+// }else if (hour<18){
+//     greeting = "Good Afternoon⛅"
+// }else{
+//     greeting = "Good Evening🌙"
+// }
+// document.getElementById("greeting").innerText = greeting
